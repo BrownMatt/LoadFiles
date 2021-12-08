ee6d97fc88d2b5766312e658334f59465367afde
mbrown <mbrown@hanys.org>
2021-12-08 16:59:48 -0500

    updates

--- LoadFiles/Program.cs
+++ LoadFiles/Program.cs
@@ -4,32 +4,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.IO;
 using ExcelDataReader;
 
 namespace LoadFiles
 {
+    public interface IFileLoader
+    {
+        // Called once before any processing
+        void OnInit();
+        // Called once when all processing is complete
+        void OnFinish();
+        int OnCol(int col, int row, IExcelDataReader reader);
+        int OnRow(int rowNumber, int readerFieldCount, IExcelDataReader reader);
+        int OnFile(string filePath, FileInfo info);
+        int OnSheet(string sheetName);
+    }
+
+    public class TraceData : IFileLoader
+    {
+        private Action<string> _logger;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public TraceData(Action<string> logger=null)
+        {
+            _logger = logger ?? Console.WriteLine;
+        }
+
+        public void OnInit()
+        {
+            stopwatch.Start();
+        }
+
+        public void OnFinish()
+        {
+            stopwatch.Stop();
+            TimeSpan t = stopwatch.Elapsed;
+            _logger.Invoke($"Finished {DateTime.Now.ToString()} {t:hh\\:mm\\:ss}");
+        }
+
+        public int OnCol(int col, int row, IExcelDataReader reader)
+        {
+            string value = reader.GetValue(col) == null ? "null" : reader.GetValue(col).ToString();
+            string type = "null";
+            if (reader.GetValue(col) != null)
+                type = reader.GetFieldType(col).FullName;
+
+            _logger.Invoke($"Column {col.ToString()} Type {type} Value {value}");
+            return 0;
+        }
+
+        public int OnRow(int rowNumber, int readerFieldCount, IExcelDataReader reader)
+        {
+            if (rowNumber == 0)
+            {
+                if (null != reader.HeaderFooter)
+                {
+                    HeaderFooter h = reader.HeaderFooter;
+                    _logger.Invoke($"Contains header!");
+                }
+            }
+            _logger.Invoke($"Row {rowNumber.ToString()} FieldCount {readerFieldCount}");
+            return 0;
+        }
+
+        public int OnFile(string filePath, FileInfo info)
+        {
+            _logger.Invoke($"File {info.Name}");
+            return 0;
+        }
+
+        public int OnSheet(string sheetName)
+        {
+            _logger.Invoke($"Sheet '{sheetName}'");
+            return 0;
+        }
+    }
+
+    
+
+    #region FileLoaderClass
 
     public class FileLoader
     {
         private string _logFolder;
         private string _loadFolder;
+        private IFileLoader _loader;
+        private Action<string> _logger;
 
-        public FileLoader(string logFolder="", string loadFolder="")
+        public FileLoader(string logFolder = "", string loadFolder = "", IFileLoader loader = null, Action<string> logger = null)
         {
             _logFolder = (!string.IsNullOrEmpty(logFolder)) ? logFolder : ConfigurationManager.AppSettings["LogPath"];
             _loadFolder = (!string.IsNullOrEmpty(loadFolder)) ? loadFolder : ConfigurationManager.AppSettings["LoadDirectory"];
             if (null == _loadFolder)
             {
-                var os = Environment.OSVersion;
                 _loadFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
                 _loadFolder = Path.Combine(_loadFolder, "incoming");
             }
+
+            _logger = logger ?? Console.WriteLine;
+            _loader = loader ?? new TraceData(_logger);
         }
 
-     
+
         public int LoadFiles()
         {
+            _loader.OnInit();
+
             int count = 0;
 
             string searchPattern = ConfigurationManager.AppSettings["SearchPattern"];
@@ -41,12 +124,14 @@ namespace LoadFiles
             // Now process files in load folder
             foreach (var file in matchingFiles)
             {
-                Console.WriteLine($"Processing file '{file}'");
+                _logger.Invoke($"Processing file '{file}'");
                 if (0 == LoadFile(file)) count++;
             }
 
+            _loader.OnFinish();
+
             return count;
-                
+
         }
 
         private List<string> GetMatchingFiles(string rootFolder, bool searchSubfolders, string searchPattern)
@@ -60,12 +145,12 @@ namespace LoadFiles
                 try
                 {
                     string[] filesInCurrent = Directory.GetFiles(currentFolder, searchPattern, System.IO.SearchOption.TopDirectoryOnly);
-                    if(filesInCurrent.Length > 0)
+                    if (filesInCurrent.Length > 0)
                         files.AddRange(filesInCurrent);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    Console.WriteLine($"Directory.GetFiles Exception {ex.ToString()}");
+                    _logger.Invoke($"Directory.GetFiles Exception {ex.ToString()}");
                 }
                 try
                 {
@@ -80,7 +165,7 @@ namespace LoadFiles
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Directory.GetDirectories Exception {ex.ToString()}");
+                    _logger.Invoke($"Directory.GetDirectories Exception {ex.ToString()}");
                 }
             }
             return files;
@@ -93,7 +178,7 @@ namespace LoadFiles
 
 
             FileInfo info = new FileInfo(filePath);
-            Console.WriteLine($"Extension '{info.Extension}' Size={info.Length.ToString()}");
+            _logger.Invoke($"Extension '{info.Extension}' Size={info.Length.ToString()}");
             if (0 != OnFile(filePath, info)) return retVal;
 
             switch (info.Extension.ToLower())
@@ -123,10 +208,10 @@ namespace LoadFiles
                 //var cfg = new ExcelReaderConfiguration(){}
                 using (var reader = ExcelReaderFactory.CreateReader(stream))
                 {
-                    Console.WriteLine($"{info.Name} contains {reader.ResultsCount.ToString()} sheets");
+                    _logger.Invoke($"{info.Name} contains {reader.ResultsCount.ToString()} sheets");
                     do
                     {
-                        if(0 == OnSheet(reader.Name))
+                        if (0 == OnSheet(reader.Name))
                         {
                             int row = 0;
                             while (reader.Read())
@@ -158,7 +243,7 @@ namespace LoadFiles
                 };
                 using (var reader = ExcelReaderFactory.CreateCsvReader(stream, cfg))
                 {
-                    Console.WriteLine($"{info.Name} contains {reader.ResultsCount.ToString()} sheets");
+                    _logger.Invoke($"{info.Name} contains {reader.ResultsCount.ToString()} sheets");
                     do
                     {
                         if (0 == OnSheet(reader.Name))
@@ -184,53 +269,48 @@ namespace LoadFiles
         }
 
 
-        private int OnCol(int col, int row, IExcelDataReader reader)
-        {
-            string value = reader.GetValue(col) == null ? "null" : reader.GetValue(col).ToString();
-            string type = "null";
-            if (reader.GetValue(col) != null)
-                type = reader.GetFieldType(col).FullName;
 
-            Console.WriteLine($"Column {col.ToString()} Type {type} Value {value}");
-            return 0;
+
+        public int OnCol(int col, int row, IExcelDataReader reader)
+        {
+            return _loader.OnCol(col, row, reader);
         }
 
-        private int OnRow(int rowNumber, int readerFieldCount, IExcelDataReader reader)
+        public int OnRow(int rowNumber, int readerFieldCount, IExcelDataReader reader)
         {
-            if (rowNumber == 0)
-            {
-                if (null != reader.HeaderFooter)
-                {
-                    HeaderFooter h = reader.HeaderFooter;
-                    Console.WriteLine($"Contains header!");
-                }
-            }
-            Console.WriteLine($"Row {rowNumber.ToString()} FieldCount {readerFieldCount}");
-            return 0;
+            return _loader.OnRow(rowNumber, readerFieldCount, reader);
         }
 
         // return non zero to skip the file
         // or zero to process file
-        private int OnFile(string filePath, FileInfo info)
+        public int OnFile(string filePath, FileInfo info)
         {
-            Console.WriteLine($"File {info.Name}");
-            return 0;
+            return _loader.OnFile(filePath, info);
         }
 
         // return non zero to skip the sheet
         // or zero to process sheet
-        private int OnSheet(string sheetName)
+        public int OnSheet(string sheetName)
         {
-            Console.WriteLine($"Sheet '{sheetName}'");
-            return 0;
+            return _loader.OnSheet(sheetName);
         }
     }
+
+    #endregion
 
     class Program
     {
         static void Main(string[] args)
         {
-            FileLoader fl = new FileLoader();
+            IFileLoader l = null;
+            switch (Environment.OSVersion.VersionString.ToLower())
+            {
+                case string m when m.Contains("windows"):
+                    l = new LoadDataToSqlServer(ConfigurationManager.AppSettings["SqlSvrCon"]);
+                    break;
+            }
+
+            FileLoader fl = new FileLoader(loader: l);
             fl.LoadFiles();
             
             if(System.Diagnostics.Debugger.IsAttached)
@@ -240,4 +320,79 @@ namespace LoadFiles
             }
         }
     }
+
+
+    public class LoadDataToSqlServer : IFileLoader
+    {
+        private Action<string> _logger;
+        private Stopwatch stopwatch = new Stopwatch();
+        private SqlConnection connection;
+        private string currentFile;
+        private string currentSheet;
+
+        public LoadDataToSqlServer(string connectionString, Action<string> logger = null)
+        {
+            _logger = logger ?? Console.WriteLine;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void OnInit()
+        {
+            stopwatch.Start();
+        }
+
+        public void OnFinish()
+        {
+            connection?.Close();
+
+            stopwatch.Stop();
+            TimeSpan t = stopwatch.Elapsed;
+            _logger.Invoke($"Finished {DateTime.Now.ToString()} {t:hh\\:mm\\:ss}");
+        }
+
+        public int OnCol(int col, int row, IExcelDataReader reader)
+        {
+            string value = reader.GetValue(col) == null ? "null" : reader.GetValue(col).ToString();
+            string type = "null";
+            if (reader.GetValue(col) != null)
+                type = reader.GetFieldType(col).FullName;
+
+            _logger.Invoke($"Column {col.ToString()} Type {type} Value {value}");
+            return 0;
+        }
+
+        public int OnRow(int rowNumber, int readerFieldCount, IExcelDataReader reader)
+        {
+            if (rowNumber == 0)
+            {
+                if (null != reader.HeaderFooter)
+                {
+                    HeaderFooter h = reader.HeaderFooter;
+                    _logger.Invoke($"Contains header!");
+                }
+            }
+            _logger.Invoke($"Row {rowNumber.ToString()} FieldCount {readerFieldCount}");
+            return 0;
+        }
+
+        public int OnFile(string filePath, FileInfo info)
+        {
+            currentFile = filePath;
+            _logger.Invoke($"File {info.Name}");
+            return 0;
+        }
+
+        public int OnSheet(string sheetName)
+        {
+            currentSheet = sheetName;
+            // check if we are reloading or loading
+            // Do a file hash and check if it changed
+
+
+            _logger.Invoke($"Sheet '{sheetName}'");
+            return 0;
+        }
+    }
+
 }
